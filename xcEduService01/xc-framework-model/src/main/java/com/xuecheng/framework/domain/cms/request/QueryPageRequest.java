fe336e94b287ebe680e59bfd3d48acbb3622222f
lao lu <email@laolu.com>
2019-03-17 15:00:00 +0800

    swagger接口开发添加

--- xcEduService01/xc-framework-model/src/main/java/com/xuecheng/framework/domain/cms/request/QueryPageRequest.java
+++ xcEduService01/xc-framework-model/src/main/java/com/xuecheng/framework/domain/cms/request/QueryPageRequest.java
@@ -14,15 +14,19 @@ public class QueryPageRequest extends RequestData{
     //接收页面查询的查询条件
     //站点id
     //站点id
-    @ApiModelProperty("站点id")
+    @ApiModelProperty("站点id") //用对象接收参数时描述对象的一个子段
     private String siteId;
     //页面ID
+    @ApiModelProperty("页面ID")
     private String pageId;
     //页面名称
+    @ApiModelProperty("页面名称")
     private String pageName;
     //别名
+    @ApiModelProperty("别名")
     private String pageAliase;
     //模版id
+    @ApiModelProperty("模板ID")
     private String templateId;
     //....
 }
--- xcEduService01/xc-service-api/src/main/java/com/xuecheng/api/cms/CmsPageControllerApi.java
+++ xcEduService01/xc-service-api/src/main/java/com/xuecheng/api/cms/CmsPageControllerApi.java
@@ -7,13 +7,19 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 
+import javax.naming.Name;
+
 @Api(value="cms页面管理接口",description = "cms页面管理接口，提供页面的增、删、改、查")
 public interface CmsPageControllerApi {
     //页面查询
     @ApiOperation("分页查询页面列表")
-    @ApiImplicitParams({
+   /*@ApiImplicitParams({
             @ApiImplicitParam(name="page",value = "页码",required=true,paramType="path",dataType="int"),
             @ApiImplicitParam(name="size",value = "每页记录数",required=true,paramType="path",dataType="int")
+    })*/
+    @ApiImplicitParams({
+        @ApiImplicitParam(name="page",value = "页码",required = true ,paramType = "path",dataType = "int"),
+            @ApiImplicitParam(name="size",value = "一页显示数量",required = true ,paramType = "path",dataType = "int")
     })
     public QueryResponseResult findList(int page, int size, QueryPageRequest queryPageRequest);
 }
