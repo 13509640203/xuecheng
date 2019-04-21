cee420a06c52a9d6351377cde9fb1f83ebbc40bf
lao lu <email@laolu.com>
2019-04-22 00:56:14 +0800

    第六天：添加课时计划

--- xcEduService01/xc-service-api/src/main/java/com/xuecheng/api/course/CourseControllerApi.java
+++ xcEduService01/xc-service-api/src/main/java/com/xuecheng/api/course/CourseControllerApi.java
@@ -1,6 +1,8 @@
 package com.xuecheng.api.course;
 
+import com.xuecheng.framework.domain.course.Teachplan;
 import com.xuecheng.framework.domain.course.ext.TeachplanNode;
+import com.xuecheng.framework.model.response.ResponseResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
@@ -8,5 +10,6 @@ import io.swagger.annotations.ApiOperation;
 public interface CourseControllerApi {
     @ApiOperation("查询课程计划")
     public TeachplanNode findCourseList(String courseId);
-
+    @ApiOperation("添加课程计划")
+    public ResponseResult addCourseTeachplan(Teachplan teachplan);
 }
--- xcEduService01/xc-service-manage-course/src/main/java/com/xuecheng/manage_course/service/CourseService.java
+++ xcEduService01/xc-service-manage-course/src/main/java/com/xuecheng/manage_course/service/CourseService.java
@@ -1,17 +1,90 @@
 package com.xuecheng.manage_course.service;
 
+import com.xuecheng.framework.domain.course.CourseBase;
+import com.xuecheng.framework.domain.course.Teachplan;
 import com.xuecheng.framework.domain.course.ext.TeachplanNode;
+import com.xuecheng.framework.exception.ExceptionCast;
+import com.xuecheng.framework.model.response.CommonCode;
+import com.xuecheng.framework.model.response.ResponseResult;
+import com.xuecheng.manage_course.dao.CourseBaseRepository;
 import com.xuecheng.manage_course.dao.TeachplanMapper;
+import com.xuecheng.manage_course.dao.TeanchplanRepository;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.security.cert.X509Certificate;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CourseService {
-  @Autowired
-  TeachplanMapper teachplanMapper;
+   @Autowired
+   TeachplanMapper teachplanMapper;
+   @Autowired
+    TeanchplanRepository teanchplanRepository;
+    @Autowired
+    CourseBaseRepository courseBaseRepository;
+  //查询课程计划
   public TeachplanNode findTeachplanList(String courseId){
       TeachplanNode courseList = teachplanMapper.findCourseList(courseId);
       return  courseList;
   }
+  //添加课程计划
+    @Transactional //CRUD都需要添加事务
+    public ResponseResult addCourseTeachplan(Teachplan teachplan) {
+        if(teachplan==null|| StringUtils.isEmpty(teachplan.getCourseid())||StringUtils.isEmpty(teachplan.getPname())){
+            ExceptionCast.cast(CommonCode.INVALID_PARAM);//非法参数
+        }
+        String courdeId = teachplan.getCourseid();
+        String parentid = teachplan.getParentid();
+        if(StringUtils.isEmpty(parentid)||parentid.length()<=0){//父id为空,没有填父上级节点
+            parentid = this.getTeachplanParentid(courdeId);
+        }
+        //父节点查询出页面在某一个课程下添加课程计划时改课程的id
+        Optional<Teachplan> byId = teanchplanRepository.findById(parentid);
+        if(!byId.isPresent()){
+            ExceptionCast.cast(CommonCode.INVALID_PARAM);//非法参数
+        }
+        Teachplan teachplan2 = byId.get();
+        String parentidGrade=teachplan2.getGrade();
+        //准备添加课程计划
+        Teachplan teachplan1 = new Teachplan();
+        teachplan1.setCourseid(courdeId);
+        teachplan1.setParentid(parentid);//父节点
+        teachplan1.setPname(teachplan.getPname());
+        teachplan1.setStatus("0");
+        if(parentidGrade.equals("1")){
+            teachplan1.setGrade("2");
+        }else {
+            teachplan1.setGrade("3");
+        }
+        teanchplanRepository.save(teachplan1);
+        return new ResponseResult(CommonCode.SUCCESS);
+    }
+    //获取课程根节点，如果没有则添加
+    private String getTeachplanParentid(String courseId){//父id为空,没有填父上级节点,所以Parentid=0
+        List<Teachplan> byCourseidAndParentid = teanchplanRepository.findByCourseidAndParentid(courseId, "0");
+        Optional<CourseBase> byId = courseBaseRepository.findById(courseId);
+        if(!byId.isPresent()){
+            ExceptionCast.cast(CommonCode.INVALID_PARAM);//非法参数
+        }
+        CourseBase courseBase = byId.get();
+
+        //需要添加
+        if(byCourseidAndParentid==null||byCourseidAndParentid.size()<=0){
+            Teachplan teachplan = new Teachplan();
+            teachplan.setCourseid(courseId);
+            teachplan.setGrade("1");//一级
+            teachplan.setParentid("0");
+            teachplan.setStatus("0");//未发布
+            teachplan.setPname(courseBase.getName());
+            teanchplanRepository.save(teachplan);
+            return teachplan.getId();//以id作为新添加的课程计划父id
+        }
+        //不为空
+        return  byCourseidAndParentid.get(0).getId();
+    }
 
 }
